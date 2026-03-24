b84e9955fba90d6ab489e409ee0004f8561d4ca2
Safar <safar@local>
2026-03-24 06:15:35 +0000

    android: pin OkHttp 4 for ApiService Kotlin extensions (fix compile)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Retrofit 2.9 pulls OkHttp 3.x; ApiService uses OkHttp 4 Kotlin extensions (toMediaType, toRequestBody, body property).
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.java-websocket:Java-WebSocket:1.5.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
